d085026c8cc62a8e5f94793a11989be6bef0ea65
Jesse Yeboah <jesseyeboah16@gmail.com>
2024-10-06 23:10:05 +0000

    task 0

---
+++ 0-pascal_triangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def pascal_triangle(n):
+    matrix = []
+
+    if n <= 0:
+        return matrix
+    matrix = [[1]]  # Start with the first row of Pascal's triangle
+
+    for i in range(1, n):
+        temp = [1]  # Every row starts with 1
+        for j in range(1, len(matrix[i - 1])):  # Loop through the previous row
+            temp.append(matrix[i - 1][j - 1] + matrix[i - 1][j])  # Add adjacent elements
+        temp.append(1)  # Every row ends with 1
+        matrix.append(temp)  # Add the row to the triangle
+
+    return matrix
